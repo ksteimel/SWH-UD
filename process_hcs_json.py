d25402343caa8662f21758a96e5d3b66087c9c6e
ksteimel <ksteimel@iu.edu>
2019-02-22 21:47:09 -0500

    Added Fran's script for processing hcs json

---
+++ process_hcs_json.py
@@ -0,0 +1,37 @@
+import json, sys
+total = 0
+tks = 0
+for f in sys.argv[1:]:
+	s_no = 0
+	fd = open(f, 'r')
+	doc = json.load(fd)
+	for s in doc['kwic']:
+		idx = 1
+		ws = []
+		sent = ''
+		for w in s['tokens']:
+#			print(w)
+			#{'word': 'Mapenzi', 'gloss': 'Favourite', 'pos': 'N', 'syntax': '@<NH', 'lemma': 'penzi', 'msdextra': '_', 'lex': '|penzi..nn.1|', 'msd': '5/6-PL'}
+			
+			misc = '_'
+			if 'lex' in w:
+				misc = 'Lex=' + w['lex']
+				if 'gloss' in w and type(w['gloss']) != type(None):
+					misc = misc + '|Gloss=' + w['gloss']
+
+			tag = w['pos'] + '|'+w['msd']
+			tag = tag.strip('|').replace('|_', '').replace(' ','|')
+			ws.append((idx, w['word'], w['lemma'], '_', tag.strip('|'), '_', '_', w['syntax'], '_', '_'))
+			sent = sent + w['word'] + ' '
+			idx += 1
+			tks += 1
+		print('# sent_id = %s:%d' % (f, s_no))
+		print('# text = %s' % (sent))
+		for w in ws:
+			print('"<%s>"\n\t"%s" %s %s #%d->' % (w[1], w[2], w[4].replace('|', ' '), w[7], w[0]))
+#                        print('%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % w)
+		print()
+		s_no += 1
+		total += 1
+
+print(tks, total, file=sys.stderr)
